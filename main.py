f37d239ff8fd8224458d2830c4864a02d293199c
mnsmns48 <getzboy@list.ru>
2025-02-23 21:52:43 +0300

    add retry

--- main.py
+++ main.py
@@ -8,10 +8,9 @@ def main():
     sessions = DbSessions(secret=env)
     Base.metadata.create_all(sessions.local_engine)
     Base.metadata.create_all(sessions.ssh_engine)
-    with sessions.local() as local_session:
-        truncate_table(session=local_session, table=StockTable3.__table__)
-    with sessions.ssh() as ssh_session:
-        truncate_table(session=ssh_session, table=StockTable3.__table__)
+    for session_factory in [sessions.local, sessions.ssh]:
+        with session_factory() as session:
+            truncate_table(session=session, table=StockTable3.__table__)
 
 
 if __name__ == '__main__':
